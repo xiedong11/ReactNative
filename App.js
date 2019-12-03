d8231873103266954b2ea4d7e22eee97e964520c
xiedong <xieguodong@chinanews.com.cn>
2019-12-03 17:39:54 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  Text,
+} from 'react-native';
+
+
+
+const App: () => React$Node = () => {
+  return (
+      <Text>
+        dfsafdfdsfds
+      </Text>
+
+  );
+};
+
+
+
+export default App;
